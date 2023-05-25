30d01da1414e06642da225f054e1b04ae5ec4b22
Guilherme Anjos <49658386+Guilherme-dev15@users.noreply.github.com>
2023-05-25 00:47:40 -0300

    Add files via upload

---
+++ POKEDEX/assets/js/poke-api.js
@@ -0,0 +1,33 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+    pokemon.stats = pokeDetail.stats.map((stat) => ({
+        name: stat.stat.name,
+        base_stat: stat.base_stat,
+    }));
+    pokemon.types = types;
+    pokemon.type = type;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    return pokemon;
+}
+
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
